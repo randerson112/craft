57ad2547b3830ea8dfa21d48b62ccf13b8370a51
Ryan Anderson <ryanandersont112@gmail.com>
2025-08-14 01:06:45 -0400

    Rework run command so it checks current directory and build directory

---
+++ include/run.h
@@ -0,0 +1,7 @@
+#ifndef RUN_H
+#define RUN_H
+
+// Runs given executable, checking build directory and current directory
+int runExecutable(const char* cwd, const char* executableName);
+
+#endif // RUN_H
