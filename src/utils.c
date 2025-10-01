6eef327cf3ae56c861e78de3621d7bf790381140
Ryan Anderson <ryanandersont112@gmail.com>
2025-10-01 00:31:20 -0400

    Add checks if directories or files already exist when creating a new project

--- src/utils.c
+++ src/utils.c
@@ -71,78 +71,89 @@ const char* getBaseName(const char* path)
 
 int createCppProject(const char* path, const char* name)
 {
-    // Make include directory
+    // Make include directory if one does not exist
     char includeDir[256];
     snprintf(includeDir, sizeof(includeDir), "%s/include", path);
-    if (mkdir(includeDir, 0755) == 0)
+    if (!dirExists(includeDir))
     {
-        printf("Generated include directory\n");
-    }
-    else
-    {
-        fprintf(stderr, "Failed to create include directory\n");
-        return -1;
+        if (mkdir(includeDir, 0755) == 0)
+        {
+            printf("Generated include directory\n");
+        }
+        else
+        {
+            fprintf(stderr, "Failed to create include directory\n");
+            return -1;
+        }
     }
 
-    // Make src directory
+    // Make src directory if it does not exist
     char srcDir[256];
     snprintf(srcDir, sizeof(srcDir), "%s/src", path);
-    if (mkdir(srcDir, 0755) == 0)
+    if (!dirExists(srcDir))
     {
-        printf("Generated src directory\n");
-    }
-    else
-    {
-        fprintf(stderr, "Failed to create src directory\n");
-        return -1;
+        if (mkdir(srcDir, 0755) == 0)
+        {
+            printf("Generated src directory\n");
+        }
+        else
+        {
+            fprintf(stderr, "Failed to create src directory\n");
+            return -1;
+        }
     }
 
-    // Make default main.cpp
+    // Make default main.cpp if there is none
     char mainFilePath[256];
     snprintf(mainFilePath, sizeof(mainFilePath), "%s/src/main.cpp", path);
 
-    FILE* mainFile = fopen(mainFilePath, "w");
-    if (mainFile == NULL)
+    if (!fileExists(mainFilePath))
     {
-        fprintf(stderr, "Failed to generate main.cpp\n");
-        return -1;
+        FILE* mainFile = fopen(mainFilePath, "w");
+        if (mainFile == NULL)
+        {
+            fprintf(stderr, "Failed to generate main.cpp\n");
+            return -1;
+        }
+
+        // Default source file contents if no corresponding header
+        fprintf(mainFile, "#include <iostream>\n\n");
+        fprintf(mainFile, "int main(int argc, char* argv[]) {\n");
+        fprintf(mainFile, "   std::cout << \"This is my %s project!\" << std::endl;\n", name);
+        fprintf(mainFile, "}");
+        printf("Generated main.cpp\n");
     }
 
-    // Default source file contents if no corresponding header
-    fprintf(mainFile, "#include <iostream>\n\n");
-    fprintf(mainFile, "int main(int argc, char* argv[]) {\n");
-    fprintf(mainFile, "   std::cout << \"This is my %s project!\" << std::endl;\n", name);
-    fprintf(mainFile, "}");
-    printf("Generated main.cpp\n");
-
-    // Make default CMakeLists.txt
-    // Project name
-    char projectName[64];
-    strcpy(projectName, name);
-    projectName[0] = toupper(projectName[0]);
-
-    // Executable name
-    char executableName[64];
-    strcpy(executableName, name);
-
-    // Create CMakeLists.txt file
+    // Make default CMakeLists.txt if none exists
     char cmakePath[256];
     snprintf(cmakePath, sizeof(cmakePath), "%s/CMakeLists.txt", path);
 
-    FILE* cmakeFile = fopen(cmakePath, "w");
-    if (cmakeFile == NULL)
+    if (!fileExists(cmakePath))
     {
-        fprintf(stderr, "Failed to generate CMakeLists.txt\n");
-        return -1;
-    }
+        // Project name
+        char projectName[64];
+        strcpy(projectName, name);
+        projectName[0] = toupper(projectName[0]);
 
-    // Write default content
-    fprintf(cmakeFile, "cmake_minimum_required(VERSION 3.10)\n");
-    fprintf(cmakeFile, "project(%s)\n\n", projectName);
-    fprintf(cmakeFile, "set(CMAKE_CXX_STANDARD 17)\n\n");
-    fprintf(cmakeFile, "include_directories(include)\n\n");
-    fprintf(cmakeFile, "add_executable(%s src/main.cpp)", executableName);
-    printf("Generated CMakeLists.txt\n");
+        // Executable name
+        char executableName[64];
+        strcpy(executableName, name);
+
+        FILE* cmakeFile = fopen(cmakePath, "w");
+        if (cmakeFile == NULL)
+        {
+            fprintf(stderr, "Failed to generate CMakeLists.txt\n");
+            return -1;
+        }
+
+        // Write default content
+        fprintf(cmakeFile, "cmake_minimum_required(VERSION 3.10)\n");
+        fprintf(cmakeFile, "project(%s)\n\n", projectName);
+        fprintf(cmakeFile, "set(CMAKE_CXX_STANDARD 17)\n\n");
+        fprintf(cmakeFile, "include_directories(include)\n\n");
+        fprintf(cmakeFile, "add_executable(%s src/main.cpp)", executableName);
+        printf("Generated CMakeLists.txt\n");
+    }
 
     return 0;
 }
